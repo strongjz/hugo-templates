85265b3227abe326688e3a7ee73fea279f428281
James Strong <strong.james.e@gmail.com>
2019-07-20 17:39:14 -0400

    trying to make pdf

--- create.sh
+++ create.sh
@@ -5,59 +5,106 @@
 #
 
 CODE_DIR="$HOME/Documents/code/"
+PDF='false'
+CREATE='false'
 
 create() {
 
-cd ${CODE_DIR} || exit 1
+    cd ${CODE_DIR} || echo "Code directory: ${CODE_DIR} does not exist" && exit 1
 
-hugo new site ${PRESENTATION}
+    hugo new site ${NAME_PRESENTATION}
 
-cd ${PRESENTATION} || exit 1
+    cd ${PRESENTATION} || exit 1
 
-git init
+    git init
 
-git submodule add git@github.com:dzello/reveal-hugo.git themes/reveal-hugo
+    git submodule add git@github.com:dzello/reveal-hugo.git themes/reveal-hugo
 
-echo "theme = \"reveal-hugo\"
-[outputFormats.Reveal]
-baseName = \"index\"
-mediaType = \"text/html\"
-isHTML = true" >  config.toml
+    echo "theme = \"reveal-hugo\"
+    [outputFormats.Reveal]
+    baseName = \"index\"
+    mediaType = \"text/html\"
+    isHTML = true" >  config.toml
 
-echo "+++
-title = \"${PRESENTATION}\"
-outputs = [\"Reveal\"]
-+++
+    echo "+++
+    title = \"${NAME_PRESENTATION}\"
+    outputs = [\"Reveal\"]
+    +++
 
-# Hello world!
+    # Hello world!
 
-This is my first slide." > content/_index.md
+    This is my first slide." > content/_index.md
 
-curl -X GET -skL -o Makefile https://gist.githubusercontent.com/strongjz/d703c05a0a79823ecf8b77529dbbf7b7/raw/d51964e81aac77baf5c1155fcbafc775e48eeafa/Makefile
+    hugo_makefile
 
-make serve
+    make serve
 }
 
+hugo_makefile() {
+    echo "Downloading Hugo Makefile"
+
+    curl -X GET -skL -o Makefile https://gist.githubusercontent.com/strongjz/d703c05a0a79823ecf8b77529dbbf7b7/raw/d51964e81aac77baf5c1155fcbafc775e48eeafa/Makefile
+
+}
+
+install_decktape() {
+
+    echo "Installing Decktape"
+
+    npm install -g decktape
+}
+
+# Creates the reveal presentation as a PDF
+pdf() {
+
+echo "Generating PDF"
+
+if [[ -e Makefile ]]; then
+    hugo_makefile
+fi
+
+make serve &
+
+decktape reveal http://localhost:1313/${NAME_PRESENTATION} ${NAME_PRESENTATION}.pdf
+
+}
 
 usage() {
 
-    echo "Usage: ./create.sh -p NAME_PRESENTATION -c CODE_DIR
+    echo "Usage: ./create.sh -c -n NAME_PRESENTATION -d CODE_DIR
 
-    -p - Name of the presentation you want to create
+    -n - Name of the presentation you want to create
 
-    -c - Directory to store it, defaults to $HOME/Documents/code/, this will place -p there " 1>&2;
+    -d - Directory to store it, defaults to $HOME/Documents/code/, this will place -p there
+
+    -c - Create new presentation
+
+    -p - Generate PDF of presentation" 1>&2;
+
     exit 1;
 }
 
-while getopts ":h:p:c:" o; do
+while getopts ":h:n:d:c:p" o; do
     case "${o}" in
-        p)
-            PRESENTATION=${OPTARG}
+        n)
+            NAME_PRESENTATION=${OPTARG}
             ;;
-        c)
+        d)
             CODE_DIR=${OPTARG}
             ;;
+        p)
+            PDF='true'
+            ;;
+        c)
+            CREATE='true'
+            ;;
         *)
+            usage
+            ;;
+        '?')
+            usage
+            ;;
+        ':')
             usage
             ;;
         h)
@@ -65,10 +112,33 @@ while getopts ":h:p:c:" o; do
             ;;
     esac
 done
-shift $((OPTIND-1))
 
-if [[ -z "${PRESENTATION}" ]]; then
+if [[ $OPTIND -eq 1 ]]; then
+    echo "No options were passed"
     usage
 fi
 
-create
+shift $((OPTIND-1))
+
+if [[ -z "${NAME_PRESENTATION}" ]]; then
+    echo "Presentation name not defined"
+    usage
+fi
+
+if ${CREATE}; then
+    echo "Creating NAME_PRESENTATION: ${NAME_PRESENTATION}"
+    create
+fi
+
+if ${PDF}; then
+    echo "Generating PDF for ${NAME_PRESENTATION}"
+    pdf
+fi
+
+echo "Issue starting"
+usage
+
+
+
+
+
